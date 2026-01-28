1a5b4444458a13362a186a95e9b80ca4f206f4e5
hajosboss <akurvaanydad@gmail.com>
2026-01-28 11:43:24 +0100

    Add project files.

---
+++ kitalalo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+var words = System.IO.File.ReadAllLines("szavak.txt");
+var rnd = new Random();
+var target = words[rnd.Next(words.Length)];
+var targetLower = target.ToLower();
+var mask = new string('.', target.Length).ToCharArray();
+int attempts = 0;
+
+while (true)
+{
+    Console.Write("Tipp: ");
+    var line = Console.ReadLine();
+    if (line == null) break;
+    var guess = line.Trim();
+    if (guess == "") continue;
+    if (guess.ToLower() == "stop") break;
+
+    attempts++;
+    if (guess.ToLower() == targetLower)
+    {
+        Console.WriteLine(target);
+        Console.WriteLine(attempts);
+        break;
+    }
+
+    var g = guess.ToLower();
+    for (int i = 0; i < target.Length && i < g.Length; i++)
+        if (g[i] == targetLower[i]) mask[i] = target[i];
+
+    Console.WriteLine(new string(mask));
+}
